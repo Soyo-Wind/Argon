066ce8acd7073cc63cfbd89771069a682199512b
Soyo-Wind <ainaba601@gmail.com>
2025-08-25 19:20:57 +0900

    move split

--- ll1/owen(main).cs
+++ ll1/owen(main).cs
@@ -1,6 +1,4 @@
 ﻿
-using System.Text;
-
 namespace ll1;
 
 class Owen
@@ -13,9 +11,9 @@ class Owen{
 ";
     public static void Main(string[] args)
     {
-        string input = @"%(main)=rin[<""asdf"">]";
-        string[] lines = split(input.Replace("\r", ""));
-        VEx.codex(lines);
+        string input = @"%(main)=rin[<""asdf"">]/";
+        VEx.codex(input.Replace("\r", ""));
+        cscode += "}";
         Console.WriteLine(cscode);
     }
 
@@ -23,35 +21,5 @@ class Owen{
     {
         cscode += code + "\n";
     }
-    
-    private static string[] split(string expr)
-    {
-        List<string> parts = new();
-        StringBuilder current = new(expr);
-        for (int pos = 0; pos < expr.Length; pos++)
-        {
-            if (expr[pos] == '<')
-            {
-                for (int depth = 1; depth > 0; pos++)
-                {
-                    if (pos >= expr.Length)
-                        throw new Exception("構文違い(<>が閉じていない)");
-                    if (expr[pos] == '<')
-                        depth++;
-                    else if (expr[pos] == '>')
-                        depth--;
-                }
-            }
-            else if (expr[pos] == '"')
-            {
-                for (int depth = 1; expr[depth] != '"'; pos++) ;
-            }
-            else if (expr[pos] == '/')
-            {
-                parts.Add(current.ToString(0, pos).Trim());
-                current.Remove(0, pos + 1);
-            }
-        }
-        return parts.ToArray();
-    }
+
 }
--- ll1/varex.cs
+++ ll1/varex.cs
@@ -5,8 +5,10 @@ namespace ll1;
 
 class VEx
 {
-    public static void codex(string[] lines)
+    public static string codex(string raw)
     {
+        string[] lines = split(raw);
+        string code = string.Empty;
         foreach (string line in lines)
         {
             StringBuilder name = new();
@@ -22,6 +24,7 @@ class VEx
                 SkipSpace(line, ref pos);
                 for (; pos < line.Length && line[pos] != '='; name.Append(line[pos]), pos = pos >= line.Length ?
                 throw new Exception("構文違い(変数)") : pos + 1) ;
+                name = name.Replace("%", "").Replace("(", "").Replace(")", "");
                 if (line[pos] != '=')
                     throw new Exception("構文違い(=がない)");
                 for (; pos < line.Length && line[pos] != '['; _ += line[pos], pos = pos >= line.Length ?
@@ -90,10 +93,10 @@ class VEx
                     }
                     values[0] = new(NEx.numval(values[0].ToString()));
                 }
-                Owen.addCode(type switch
+                code += type switch
                 {
                     VariableType.rin => $"string {name} = {values[0]};",
-                    VariableType.vid => $"internal static {(values[0].ToString() == "nil"?"void":
+                    VariableType.vid => $"internal static {(values[0].ToString() == "nil" ? "void" :
                     values[0].ToString() switch
                     {
                         "rin" => "string",
@@ -103,20 +106,48 @@ class VEx
                         "cim" => "decimal",
                         "flo" => "float",
                         _ => throw new Exception("不明な型"),
-                    })} {name}(){{ {string.Join(", ", values)} }};",
+                    })} {name}(){{ {codex(values[2].ToString())} }};",
                     VariableType.tnil => $"bool {name} = {values[0]};",
                     VariableType.bin => $"BigInteger {name} = {values[0]};",
                     VariableType.teg => $"int {name} = {values[0]};",
                     VariableType.cim => $"decimal {name} = '{values[0]}';",
                     VariableType.flo => $"float {name} = {values[0]}f;",
                     _ => throw new Exception("不明な型"),
-                });
+                } + "\n";
             }
         }
+        return code;
     }
-    
+
     private static void SkipSpace(string expr, ref int pos)
     {
         for (; pos < expr.Length && expr[pos] == ' '; pos++) ;
+    }
+    
+    private static string[] split(string expr)
+    {
+        List<string> parts = new();
+        StringBuilder current = new(expr);
+        for (int pos = 0; pos < expr.Length; pos++)
+        {
+            if (expr[pos] == '[')
+            {
+                for (int depth = 0; depth > 0; pos++)
+                {
+                    if (pos >= expr.Length)
+                        throw new Exception("構文違い([]が閉じていない)");
+                    if (expr[pos] == '[')
+                        depth++;
+                    else if (expr[pos] == ']')
+                        depth--;
+                }
+            }
+            else if (expr[pos] == '/')
+            {
+                parts.Add(current.ToString(0, pos).Trim());
+                current.Remove(0, pos + 1);
+            }
+        }
+        return parts.ToArray();
     }
 }
